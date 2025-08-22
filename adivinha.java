502fb8d36e5ac8ee3d404c3ea2c173a12e9e6a24
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-08-22 14:02:33 -0300

    Criando o Scanner para a letra

--- adivinha.java
+++ adivinha.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class adivinha {
  
     public static void main(String[] args) {    
         System.out.println("Olá, seja bem-vindo(a)!\n");
+        System.out.println("Digite uma letra: \n");
+            Scanner scanner = new Scanner(System.in);
+            String Letra = scanner.nextLine(); 
     }    
+
 }
