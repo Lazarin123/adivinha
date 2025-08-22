fe283327fc28f692f867d4ba39beb95a93c6d0ce
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-08-22 14:05:00 -0300

    Criação das escolhas

--- adivinha.java
+++ adivinha.java
@@ -7,6 +7,19 @@ public class adivinha {
         System.out.println("Digite uma letra: \n");
             Scanner scanner = new Scanner(System.in);
             String Letra = scanner.nextLine(); 
-    }    
 
+            if (Letra == "a") {
+                System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+            } else if (Letra == "e") {
+                System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+            } else if (Letra == "i") {
+                System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+            } else if (Letra == "o") {
+                System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+            } else if (Letra == "u") {
+                System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+            } else {
+                System.out.println("Você digitou a Letra " + Letra + ", ela é uma consoante.");
+            }    
+    }
 }
