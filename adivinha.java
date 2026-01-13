a18fe9523d92e85c6f30c38229a32e757d25f2d6
Samuel Lazarin <128560645+Lazarin123@users.noreply.github.com>
2026-01-13 14:03:37 -0300

    Update adivinha.java

--- adivinha.java
+++ adivinha.java
@@ -1,1 +1,27 @@
-x
+import java.util.Scanner;
+
+public class adivinha {
+ 
+    public static void main(String[] args) {    
+        System.out.println("Olá, seja bem-vindo(a)!\n");
+        System.out.println("Digite uma letra: \n");
+            Scanner scanner = new Scanner(System.in);
+            String Letra = scanner.nextLine(); 
+
+            switch (Letra) {
+                case "a": System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+                break;
+                case "e": System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+                break;
+                case "i": System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+                break;
+                case "o": System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+                break;
+                case "u": System.out.println("Você digitou a Letra " + Letra + ", ela é uma vogal.");
+                    break;
+                default:
+                    System.out.println("Você digitou a Letra " + Letra + ", ela é uma consoante.");
+                    break;
+            }    
+    }
+}
